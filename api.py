613432adb2df9fd67e1fcd5bde1db4b3e020bcc1
Marcin Wylot <MarcinWylot@users.noreply.github.com>
2018-06-04 10:03:18 +0200

    Add files via upload

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+
+import requests
+import json
+from statsmodels.tsa.api import ExponentialSmoothing
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/predict")
+def predict():
+    coin = request.args.get('coin')
+    
+    url = 'https://min-api.cryptocompare.com/data/histohour?tsym=EUR&limit=300&fsym={}'.format(coin)
+    response = requests.get(url)
+
+    json_array = json.loads(response.text)
+    df = pd.DataFrame(json_array['Data'], columns = ['close','time'])
+    df.set_index('time', inplace=True)
+    
+    y = df[295:300].copy()
+    y.index = y.index +6*3600
+
+    fit1 = ExponentialSmoothing(np.asarray(df['close']) ,seasonal_periods=7 ,trend='add', seasonal='mul',).fit()
+    y['Holt_Winter'] = fit1.forecast(len(y))
+
+
+    return y['Holt_Winter'].to_json()
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
